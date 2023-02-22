17282ddc2ecd517d8c21e4e0d9f1d80625da6ad6
Adson <adsontosoares@gmail.com>
2023-02-22 13:02:28 -0300

    Adicionando o cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { routes } from "./routers";
 import { TMP_FOLDER } from "./config/uploads";
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
 const port = 3333
 
 app.use(routes)
+app.use(cors())
 app.use('/files', express.static( TMP_FOLDER))
 
 app.listen(port, () => console.log(`Server is Running in https://localhost:${port}`))
